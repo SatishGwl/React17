643ce00ee5093e2f162435d54ed4e308edd32d37
Satish Sharma <Satish.Sharma@lntinfotech.com>
2023-08-31 13:07:20 +0530

    extracting theme context for component and files and create layout component

--- src/Component/Layout.js
+++ src/Component/Layout.js
@@ -1,19 +1,27 @@
-import React, { createContext, useState } from "react";
+import React, { useContext } from "react";
+import { ThemeProvider, ThemeProvider } from "../context/ThemeContext";
 export const ThemeContext = createContext();
+
 function Layout({ startingTheme, children }) {
-  const [theme, setTheme] = useState(startingTheme);
   return (
-    <ThemeContext.Provider value={(theme, setTheme)}>
-      <div
-        className={
-          theme == "light" ? "container-fluid light" : "container-fluid dark"
-        }
-      >
-        {children}
-        {/* <Header />
-        <Speakers /> */}
-      </div>
+    <ThemeContext.Provider startingTheme={startingTheme}>
+      <LayoutNoThemeProvider>{children}</LayoutNoThemeProvider>
     </ThemeContext.Provider>
+  );
+}
+
+function LayoutNoThemeProvider({ startingTheme, children }) {
+  //   const [theme, setTheme] = useState(startingTheme);
+  const { theme } = useContext(ThemeContext);
+
+  return (
+    <div
+      className={
+        theme == "light" ? "container-fluid light" : "container-fluid dark"
+      }
+    >
+      {children}
+    </div>
   );
 }
 export default Layout;
---
+++ src/context/ThemeContext.js
@@ -0,0 +1,13 @@
+import React, { createContext, useState } from "react";
+
+export const ThemeContext = createContext();
+
+function ThemeProvider({ children, startingTheme }) {
+  const [theme, setTheme] = useState(startingTheme);
+  return (
+    <ThemeContext.Provider value={(theme, setTheme)}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+export { ThemeProvider };
